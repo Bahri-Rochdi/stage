3d21459c299cde0b0e87e9e09395784005c7b30d
Bahri-Rochdi <rochdi.bahri@esprit.tn>
2021-07-01 21:18:57 +0100

    ftdctfgv

--- CPbackEnd/src/main/java/com/oga/produit/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.oga.produit.repositories;
-
-import com.oga.produit.entities.Categorie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository <Categorie, Long> {
-}
--- CPbackEnd/src/main/java/com/oga/produit/repositories/ProduitRepository.java
+++
@@ -1,7 +0,0 @@
-package com.oga.produit.repositories;
-
-import com.oga.produit.entities.Produit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProduitRepository extends JpaRepository<Produit, Long> {
-}
